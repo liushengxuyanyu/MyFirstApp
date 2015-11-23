d55c43d709abfd399569bda807dcd9171cbcaeb6
shengxu.liu <shengxu.liu@youxu.com>
2015-11-23 14:57:16 +0800

    第一次提交

---
+++ FirstApp/FirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by 升旭 刘 on 15/11/23.
+//  Copyright © 2015年 larry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
